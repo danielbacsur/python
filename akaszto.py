72123ee661bfad4f66a0b42a0555a82acb5a1e89
danielbacsur <contact@danielbacsur.com>
2022-11-21 14:09:58 +0100

    al

---
+++ akaszto.py
@@ -0,0 +1,24 @@
+import random
+ered = ['alma', 'korte', 'haha']
+szo = random.choice(ered)
+print(szo)
+
+kitalalt = []
+
+while True:
+    b = input(">> ")[0]
+    if b in szo:
+        kitalalt.append(b)
+    
+    line = ""
+    for i in szo:
+        if i in kitalalt:
+            line += i
+        else:
+            line += "#"
+    
+    breakk = True
+    for i in line:
+        if i == "#":
+            breakk = False
+    print("nzertel")
---
+++ elefant.py
@@ -0,0 +1,10 @@
+
+bemenet = input()
+
+elso = bemenet[0]
+
+utolso = bemenet[-1]
+
+hossz = len(bemenet) -2
+
+print(elso + str(hossz) + utolso)
